e3554a5674eade7dd5ca73751054bb68631a20a8
TheCaptain1810 <itspranavpise@gmail.com>
2025-03-18 16:20:35 +0000

    now I 'm understanding Go a bit

--- struct.go
+++ struct.go
@@ -3,49 +3,49 @@ package main
 import "fmt"
 
 type car struct {
-    Make string
-    Model string
+	Make  string
+	Model string
 }
 
 type truck struct {
-    car // this is embedded struck (not the same as nested struct)
-    bedSize int
+	car     // this is embedded struck (not the same as nested struct)
+	bedSize int
 }
 
 type anotherTruck struct {
-    anotherCar car
-    bedSize int
+	anotherCar car
+	bedSize    int
 }
 
-func (t truck) concat() { 
-    fmt.Printf("The make and model of truck are: %s, %s respectively\n", t.Make, t.Model)
+func (t truck) concat() {
+	fmt.Printf("The make and model of truck are: %s, %s respectively\n", t.Make, t.Model)
 }
 
-func main() {
-    newerTruck := anotherTruck {
-        bedSize: 20,
-        anotherCar: car {
-            Make: "ford",
-            Model: "2020",
-        },
-    }
+func struct_example() {
+	newerTruck := anotherTruck{
+		bedSize: 20,
+		anotherCar: car{
+			Make:  "ford",
+			Model: "2020",
+		},
+	}
 
-    fmt.Printf("The anotherTruck: %s\n", newerTruck.anotherCar.Make)
+	fmt.Printf("The anotherTruck: %s\n", newerTruck.anotherCar.Make)
 
-    newTruck := truck {
-        bedSize:  20,
-        car: car {
-            Make: "ford",
-            Model: "2028",
-        },
-    }
+	newTruck := truck{
+		bedSize: 20,
+		car: car{
+			Make:  "ford",
+			Model: "2028",
+		},
+	}
 
-    fmt.Printf("The truck: %s\n", newTruck.Make)
+	fmt.Printf("The truck: %s\n", newTruck.Make)
 
-    newCar := car {}
-    newCar.Make = "ford"
+	newCar := car{}
+	newCar.Make = "ford"
 
-    fmt.Println("The car: ", newCar.Make)
+	fmt.Println("The car: ", newCar.Make)
 
-    newTruck.concat()
+	newTruck.concat()
 }
