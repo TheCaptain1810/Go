0e8a6cd7d2a3f87221d0a49899fca76ed6669de8
TheCaptain1810 <itspranavpise@gmail.com>
2025-03-18 05:42:16 +0000

    nested and embedded structs

--- struct.go
+++ struct.go
@@ -8,6 +8,36 @@ func main() {
         Model string
     }
 
+    type truck struct {
+        car // this is embedded struck (not the same as nested struct)
+        bedSize int
+    }
+
+    type anotherTruck struct {
+        anotherCar car
+        bedSize int
+    }
+
+    newerTruck := anotherTruck {
+        bedSize: 20,
+        anotherCar: car {
+            Make: "ford",
+            Model: "2020",
+        },
+    }
+
+    fmt.Printf("The anotherTruck: %s\n", newerTruck.anotherCar.Make)
+
+    newTruck := truck {
+        bedSize:  20,
+        car: car {
+            Make: "ford",
+            Model: "2028",
+        },
+    }
+
+    fmt.Printf("The truck: %s\n", newTruck.Make)
+
     newCar := car{}
     newCar.Make = "ford"
 
